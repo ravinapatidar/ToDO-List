bbcc2ea49b12478cd1c9be832422a5f2f1faf64a
Ravina Patidar <ravinapatidar9752@gmail.com>
2025-01-07 11:55:47 +0530

    @meadia

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ let taskInput = document.querySelector(".hidden-input");
 let editTask = document.querySelector(".edit");
 let deletTask = document.querySelector(".delet");
 let hiddenContainer = document.querySelector(".hidden-container");
-
+let addItemList = document.querySelector(".addList");
+let items;
+let i=1;
 submitBtn.addEventListener("click",()=>{
     if(input.value === ""){
         alert("Please enter an task!");
@@ -13,15 +15,24 @@ submitBtn.addEventListener("click",()=>{
      hiddenContainer.classList.add("active");
     taskInput.value = input.value;
      input.value = "";
-      
+    let items = document.createElement('li');
+    items.innerText = `Task ${i++}: ${taskInput.value}`;
+    addItemList.appendChild(items);
+
 });
 
 editTask.addEventListener("click",() => {
          input.value = taskInput.value;
+         items.remove();
+         console.log(items.remove())
 });
 
 deletTask.addEventListener("click",() => {
         alert("Are you sure");
         taskInput.value = "";
         hiddenContainer.classList.remove("active");
+        items.remove();
+        
 });
+
+
